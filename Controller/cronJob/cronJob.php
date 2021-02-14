06ab27ce23eb75f018d9315c19b47efe42948a03
Alireza <alirezaeiji151379@gmail.com>
2021-02-14 12:38:32 +0330

    cronjob is started!

--- Controller/cronJob/cronJob.php
+++ Controller/cronJob/cronJob.php
@@ -1,5 +1,17 @@
 <?php
 
+function updateBlackList(){
+    $now=time();
+    $sql="DELETE FROM `black_list` WHERE `expires_at`<'$now'";
+    $db=new authDB();
+    $db->getConnection()->query($sql);
+}
+
+
+
+updateBlackList();
+
+
 
 
 
